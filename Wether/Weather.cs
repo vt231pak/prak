c8087a8a504491cf19b303a4e1e2bcf51201ead1
OptimusPrime2006 <vt231_mim@student.ztu.edu.ua>
2025-02-14 00:09:12 +0200

    Update Weather.cs

--- Wether/Weather.cs
+++ Wether/Weather.cs
@@ -1,111 +1,52 @@
-﻿using System.Net;
-using System.Text;
+using System;
+using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace WeatherLibrary
 {
     public class Weather
     {
-        private string status;
-        private double temperature;
-        private double wind;
-        private string windDirection;
-        private double humidity;
-        private double waterTemperature;
-        private byte[] image;
+        private readonly HttpClient httpClient = new HttpClient();
+        
+        public string Status { get; private set; }
+        public double Temperature { get; private set; }
+        public double Wind { get; private set; }
+        public string WindDirection { get; private set; }
+        public double Humidity { get; private set; }
+        public double WaterTemperature { get; private set; }
+        public byte[] Image { get; private set; }
 
-        public Weather(string url)
-        {
-            LoadData(url);
-        }
+        public Weather(string url) => LoadDataAsync(url).Wait();
 
-        public void LoadData(string url)
+        private async Task LoadDataAsync(string url)
         {
-            WebClient webClient = new WebClient();
-            webClient.Encoding = Encoding.UTF8;
-            string html = webClient.DownloadString(url);
-            string img = webClient.DownloadString("https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%B6%D0%B8%D1%82%D0%BE%D0%BC%D0%B8%D1%80");
-            Temperature = ExtractTemperature(html);
-            Wind = ExtractWind(html);
-            WindDirection = ExtractWindDirection(html);
-            Humidity = ExtractHumidity(html);
-            WaterTemperature = ExtractWaterTemperature(html);
-            Status = ExtractStatus(html);
-            string imgUrl = ExtractValue(img, "<div class=\"img\"> <img width=\"\\d+\" height=\"\\d+\" src=\"(//.+.jpg)\" ");
-            byte[] imageData = webClient.DownloadData(imgUrl);
-            Image = imageData;
+            try
+            {
+                string html = await httpClient.GetStringAsync(url);
+                Temperature = ExtractDoubleValue(html, "class=\"now-weather\"><span class=\"unit-temperature\".*?>([+-]?\\d+\\.?\\d*)</span>");
+                Wind = ExtractDoubleValue(html, "windSpeed\":[(\\d+\\.?\\d*)]");
+                WindDirection = ExtractValue(html, "м/c<br>([А-Яа-яІіЇї]+)</span>");
+                Humidity = ExtractDoubleValue(html, "\"humidity\":[(\\d+)]");
+                WaterTemperature = ExtractDoubleValue(html, "\"temperatureWater\":[([+-]?\\d+\\.?\\d*)]");
+                Status = ExtractValue(html, "<div class=\"now-desc\">(.+)</div><div class=\"now-info\">");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка завантаження даних: {ex.Message}");
+            }
         }
 
         private static string ExtractValue(string html, string pattern)
         {
-            Regex regex = new Regex(pattern, RegexOptions.Compiled);
-            Match match = regex.Match(html);
-            return match.Success ? match.Groups[1].Value : "помилка";
+            Match match = Regex.Match(html, pattern, RegexOptions.Compiled);
+            return match.Success ? match.Groups[1].Value : "Невідомо";
         }
 
-        private static double ExtractNumericValue(string html, string pattern)
+        private static double ExtractDoubleValue(string html, string pattern)
         {
             string value = ExtractValue(html, pattern);
-            return int.TryParse(value, out int result) ? result : 0;
-        }
-
-        private static string ExtractStatus(string html) =>
-            ExtractValue(html, "<div class=\"now-desc\">(.+)</div><div class=\"now-info\">");
-
-        private static string ExtractWindDirection(string html) =>
-            ExtractValue(html, "м/c<br>([А-Яа-яІіЇї]+)</span>");
-
-        private static double ExtractWaterTemperature(string html) =>
-            ExtractNumericValue(html, "\"temperatureWater\":[([+-]?\\d+)],");
-
-        private static double ExtractHumidity(string html) =>
-            ExtractNumericValue(html, "\"humidity\":[(\\d+)],");
-
-        private static double ExtractWind(string html) =>
-            ExtractNumericValue(html, "windSpeed\":[(\\d+)]");
-
-        private static double ExtractTemperature(string html) =>
-            ExtractNumericValue(html, "class=\"now-weather\"><span class=\"unit-temperature\" data-temperature-f=\"\\d+\">([+-]?\\d+)</span>");
-
-        public byte[] Image
-        {
-            get => image;
-            set => image = value;
-        }
-        public string Status
-        {
-            get => status;
-            set => status = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
-        public double Temperature
-        {
-            get => temperature;
-            set => temperature = value;
-        }
-
-        public double Wind
-        {
-            get => wind;
-            set => wind = value;
-        }
-
-        public string WindDirection
-        {
-            get => windDirection;
-            set => windDirection = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
-        public double Humidity
-        {
-            get => humidity;
-            set => humidity = value;
-        }
-
-        public double WaterTemperature
-        {
-            get => waterTemperature;
-            set => waterTemperature = value;
+            return double.TryParse(value, out double result) ? result : 0;
         }
     }
 }
